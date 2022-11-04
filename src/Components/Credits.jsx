0e0b742c28bfceb10a7d11de9c20d3ae894ea42f
ApVarotariya <apvarotariya17@gmail.com>
2022-11-04 17:06:36 +0530

    credits change(characters)

--- src/Components/Credits.jsx
+++ src/Components/Credits.jsx
@@ -4,6 +4,7 @@ import AliceCarousel from "react-alice-carousel";
 import { unavailable } from "./README";
 import "react-alice-carousel/lib/alice-carousel.css";
 import { LazyLoadImage } from "react-lazy-load-image-component";
+import { Link } from "react-router-dom";
 
 const API_IMG = "https://image.tmdb.org/t/p/w300/";
 
@@ -12,22 +13,32 @@ const handleDragStart = (e) => e.preventDefault();
 const Credits = ({ id, media_type }) => {
   const [credits, setCredits] = useState([]);
 
+  const fetchCredits = async () => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/${media_type}/${id}/credits?api_key=${process.env.REACT_APP_ACCESS_KEY}&language=en-US`
+    );
+    setCredits(data.cast);
+    console.log(data.cast);
+  };
+
   const items = credits?.map((c) => (
     <div className="carouselItem">
-      <LazyLoadImage
-        src={
-          c.profile_path
-            ? API_IMG + c.profile_path
-            : unavailable || c.profile_path
-        }
-        alt={c?.name}
-        onDragStart={handleDragStart}
-        className="carouselItem__img"
-      />
-      <p className="carouselItem__txt text-black my-1">
-        <strong>{c?.name || c?.original_title}</strong>
-      </p>
-      <b className="carouselItem__txt text-black">{c?.character}</b>
+      <Link to={`/person/${credits && c.id}`}>
+        <LazyLoadImage
+          src={
+            c.profile_path
+              ? API_IMG + c.profile_path
+              : unavailable || c.profile_path
+          }
+          alt={c?.name}
+          onDragStart={handleDragStart}
+          className="carouselItem__img"
+        />
+        <p className="carouselItem__txt text-black my-1">
+          <strong>{c?.name || c?.original_title}</strong>
+        </p>
+        <b className="carouselItem__txt text-black">{c?.character}</b>
+      </Link>
     </div>
   ));
 
@@ -35,14 +46,6 @@ const Credits = ({ id, media_type }) => {
     0: { items: 1 },
     568: { items: 5 },
     1024: { items: 5 },
-  };
-
-  const fetchCredits = async () => {
-    const { data } = await axios.get(
-      `https://api.themoviedb.org/3/${media_type}/${id}/credits?api_key=${process.env.REACT_APP_ACCESS_KEY}&language=en-US`
-    );
-    setCredits(data.cast);
-    console.log(data.cast);
   };
 
   useEffect(() => {
@@ -59,7 +62,7 @@ const Credits = ({ id, media_type }) => {
         disableButtonsControls
         responsive={responsive}
         items={items}
-        autoPlay
+        // autoPlay
         autoPlayInterval={600}
       />
     </>
