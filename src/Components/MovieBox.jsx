38b087fce3e9378434a98031e91eb24456440ef4
amit-swb <amit@silverwebbuzz.com>
2023-04-11 18:13:46 +0530

    changes in moviecard

--- src/Components/MovieBox.jsx
+++ src/Components/MovieBox.jsx
@@ -13,34 +13,34 @@ const MovieBox = ({ id, title, date, media_type, vote_average, poster }) => {
     <>
       <div className="col-lg-2 col-md-3 col-sm-4 col-xs-6 moviecard">
         <Card>
-          {/* <Card.Img variant="top" > */}
-          <LazyLoadImage
-            src={poster ? API_IMG + poster : unavailable}
-            className="movie-backdrop-poster"
-            alt={title}
-          />
-          {/* </Card.Img> */}
-          <Card.Body>
-            <Card.Title>
-              <h3>
-                <strong>
-                  <span>{title}</span>
-                </strong>
-                <span>{vote_average}</span>
-              </h3>
-              <div className="d-flex justify-content-between align-items-start">
-                <p style={{ fontSize: "12px" }}>
-                  <span>{dateFormat(date, "mmmm dS, yyyy")}</span>
-                </p>
-                <p style={{ fontSize: "12px" }}>
-                  {media_type === "tv" ? "TV Series" : "Movie"}
-                </p>
-              </div>
-            </Card.Title>
-            <Link to={`/${media_type}/${id}`}>
-              <Button variant="success">More Details</Button>
-            </Link>
-          </Card.Body>
+          <Link to={`/${media_type}/${id}`}>
+            <LazyLoadImage
+              src={poster ? API_IMG + poster : unavailable}
+              className="movie-backdrop-poster"
+              alt={title}
+            />
+            <Card.Body>
+              <Card.Title>
+                <h3>
+                  <strong>
+                    <span>{title}</span>
+                  </strong>
+                  <span>{vote_average}</span>
+                </h3>
+                <div className="d-flex justify-content-between align-items-start">
+                  <p style={{ fontSize: "12px" }}>
+                    <span>{dateFormat(date, "mmmm dS, yyyy")}</span>
+                  </p>
+                  <p style={{ fontSize: "12px" }}>
+                    {media_type === "tv" ? "TV Series" : "Movie"}
+                  </p>
+                </div>
+              </Card.Title>
+              <Link to={`/${media_type}/${id}`}>
+                <Button variant="success">More Details</Button>
+              </Link>
+            </Card.Body>
+          </Link>
         </Card>
       </div>
     </>
